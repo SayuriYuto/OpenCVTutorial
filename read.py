a33142d1b552e458fd4557c47df5f6afdd2bd610
SayuriYuto <dhuriamey02@gmail.com>
2022-07-23 21:55:32 +0530

    reading video

--- read.py
+++ read.py
@@ -1,5 +1,22 @@
 import cv2 as cv
 
-img = cv.imread('Resources/Photos/cat.jpg') #read image
+#READING IMAGES
+'''
+img = cv.imread('Resources/Photos/cat.jpg') #read image into an image matrix
+# print(img)
 cv.imshow('Cat',img) #display image in a window titled "Cat" with image
 cv.waitKey(0) #keep window open till a keypress
+'''
+
+#READING VIDEOS
+capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+#VideoCapture(0.) = webcam
+
+while True:
+    isTrue,frame = capture.read() #grab video frame by frame
+    cv.imshow('Video',frame) #display each frame
+    if cv.waitKey(24) & 0xFF == ord('d'): #if letter d is pressed break out of the loop, ord() returns the ascii value of char
+        break
+
+capture.release()
+cv.destroyAllWindows()
